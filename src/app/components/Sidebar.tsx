701c5c65499036d0bf936dd4a4c6e3963da9338b
Luke Chen Shui <chenshuiluke@gmail.com>
2024-04-10 02:24:07 -0500

    Fixed scrollbar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import SidebarIcon from "./SidebarIcon";
 
 const Sidebar = () => {
   return (
-    <ul className="menu bg-base-100 min-h-full overflow-scroll border-r border-r-base-300 bg-white px-2 pt-20">
+    <ul className="menu bg-base-100 overflow-y min-h-full border-r border-r-base-300 bg-white px-2 pt-20">
       <SidebarIcon href={""} active={true}>
         <CaptureTools />
       </SidebarIcon>
