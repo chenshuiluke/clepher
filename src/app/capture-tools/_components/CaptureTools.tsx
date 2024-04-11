accf5d1895679b22d27a92a8f7501349f44dbe31
Luke Chen Shui <chenshuiluke@gmail.com>
2024-04-10 22:19:34 -0500

    Added heading

--- src/app/capture-tools/_components/CaptureTools.tsx
+++ src/app/capture-tools/_components/CaptureTools.tsx
@@ -5,6 +5,9 @@ import PostEngagement from "@/app/_components/menu_icons/PostEngagement";
 const CaptureTools = () => {
   return (
     <div className="rounded-xl bg-white p-2">
+      <p className="px-4 py-2 text-sm font-bold text-slate-600/40">
+        Capture Tools
+      </p>
       <ul>
         <li role="menuitem">
           {/* TODO: Add logic for changing bg color etc when active */}
